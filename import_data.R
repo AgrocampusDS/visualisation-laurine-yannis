6bf623ca2bddee4181dce309608b8771cd9b1fe3
Yannis <yannis.barba.dev@gmail.com>
2022-11-17 18:56:59 +0100

    rename some data to join world and data and draw HDI on map

--- import_data.R
+++ import_data.R
@@ -7,3 +7,29 @@ names <- names(data)
 countries_coordinates <- read.csv("./dataset/countries_coordinates.csv", sep=",", header = TRUE)
 
 data2 <- merge(data, countries_coordinates, by="Country")
+
+## First graph with map test
+
+library('tidyverse')
+library('ggplot2')
+library("sf")
+library("rnaturalearth")
+library("rnaturalearthdata")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+world %>% ggplot() +geom_sf()
+
+## Data pre-processing : rename of certain value
+data[data$Country == "Congo (Democratic Republic of the)", "Country"] <- "Democratic Republic of the Congo"
+data[data$Country == "Congo", "Country"] <- "Republic of Congo"
+data[data$Country == "Tanzania (United Republic of)", "Country"] <- "Tanzania"
+data[data$Country == "Bolivia (Plurinational State of)", "Country"] <- "Bolivia"
+data[data$Country == "Korea (Republic of)", "Country"] <- "Republic of Korea" 
+
+complete_data <- world %>% rename(Country = name_long) %>% inner_join(data, by="Country")
+
+complete_data %>% ggplot() + geom_sf(aes(fill=HDI))
+
+
+
